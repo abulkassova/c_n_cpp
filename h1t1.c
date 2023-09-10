4f56060447edd86a3cbd249cc7115fd9869cb593
abulkassova <131253411+abulkassova@users.noreply.github.com>
2023-09-10 19:57:30 +0200

    Add files via upload

---
+++ h1t1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+float result; // The result of our calculation
+result = (3.12 + 1.23) / 5.34;
+printf("The value of 4/5 is %f\n", result);
+printf("merey");
+
+}
